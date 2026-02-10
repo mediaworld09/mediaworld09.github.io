408fca17a141af7b2c062f692d468e6ee13f5057
mediaworld09 <57594244+mediaworld09@users.noreply.github.com>
2026-02-10 14:16:38 +0200

    Update upd_list.js
    upd

--- upd_list.js
+++ upd_list.js
@@ -143,6 +143,13 @@ async function main() {
   );
 
 
+  await processPlaylist(
+    'https://mediaworld09.github.io/TV-DOSUG.m3u',
+    '  ',                                   // одна категория (строка)
+    'TV-DOSUG.m3u'
+  );
+
+
   // Добавьте сколько нужно
 
   console.log('Все плейлисты обработаны и структурированы');
